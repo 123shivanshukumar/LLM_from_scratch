4b60463a4ebbeb3c836e3d52c853647a38e1684e
Shivanshu <cs23b058@smail.iitm.ac.in>
2025-04-15 17:58:24 +0530

    add temp, notes setup gpt2

--- gpt2.py
+++ gpt2.py
@@ -3,11 +3,47 @@
 # need to reinstall stuff for this, put all the projects in this itself for avoiding repetition
 
 from dataclasses import dataclass
+###############
 import torch
 import torch.nn as nn
 from torch.nn import functional as f
 
-###############
+
+class CasualSelfAttention(nn.Module):
+    def __init__():
+        
+
+
+
+
+class MLP(nn.Module):
+    def __init__(self,config):
+        super.__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4*config.n_embd)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4*config.n_embd, config.n_embd)
+
+        def forward(self,x):
+            x = self.c_fc(x)
+            x = self.gelu(x)
+            x = self.c_proj(x)
+            return x
+
+
+class Block(nn.Module):
+    def __init__(self,config):
+        super.__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+
+    def forward(self,x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
 
 @dataclass # wth
 class GPTConfig:
@@ -24,6 +60,10 @@ class GPT(nn.Module):
         self.config = config
         self.transformer = nn.ModuleDict(
             dict(
-                wte = nn.E
+                wte = nn.Embedding(config.vocab_size , config.n_embd),
+                wpe = nn.Embedding(config.block_size, config.n_embd),
+                h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                ln_f = nn.LayerNorm(config.n_embd),
             )
         )
+
---
+++ notes.md
@@ -0,0 +1,9 @@
+# GPT2 model structure
+
+## Tokenisation
+- tikToken by GPT, sentecePiece by google (subword level tokenisation)
+- 
+(Generatively Pretrained Transformer )
+- follows decoder only transformer architecture
+- **GELU** smoother RELU : to boost performance approxiamation using tanh
+- 
